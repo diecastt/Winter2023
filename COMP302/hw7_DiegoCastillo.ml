e6443b4ce6a619b1a8b4ae47744ac1ab8a2329a9
Diego Castillo <35648211+diecastt@users.noreply.github.com>
2023-03-08 14:30:20 -0500

    Completed hw7 - COMP 302

--- COMP302/hw7_DiegoCastillo.ml
+++ COMP302/hw7_DiegoCastillo.ml
@@ -18,10 +18,11 @@ and lucas2 = {
 
 (* Question 1.3 *)
 let rec unfold (f : 'a -> 'b * 'a) (seed : 'a) : 'b stream = 
-  
+  str_map (fun (x, s) -> x) (iterate (fun (x, s) -> f s) (f seed))
   
 (* Question 1.3 *)
-  let lucas : int stream = int_stream_not_implemented 
+let lucas : int stream = 
+  unfold (fun (a, b) -> (a, (b, a + b))) (2, 1)
   
 
 
@@ -29,10 +30,19 @@ let rec unfold (f : 'a -> 'b * 'a) (seed : 'a) : 'b stream =
 
 (* Question 2.1 *)
 let rec scale (s1 : int stream) (n : int) : int stream =
-  raise NotImplemented
+  str_map (fun x -> x * n) s1
 
 let rec merge (s1 : 'a stream) (s2 : 'a stream) : 'a stream =
-  raise NotImplemented
+  {
+    head = if s1.head <= s2.head then s1.head else s2.head;
+    tail = if s1.head < s2.head then Susp (fun () -> merge (force s1.tail) s2)
+      else if s1.head > s2.head then Susp (fun () -> merge s1 (force s2.tail))
+      else Susp (fun () -> merge (force s1.tail) (force s2.tail))
+  }
 
 (* Question 2.2 *)
-let rec s = int_stream_not_implemented 
+let rec s = 
+  {
+    head = 1;
+    tail = Susp (fun () -> merge (merge (scale s 2) (scale s 3)) (scale s 5))
+  }
