55be6cbb95228bc31eb3dd40b10a6d7228cf1265
Diego <diego007.castillo@gmail.com>
2023-03-07 21:38:18 -0500

    Added hw7 - COMP 302

---
+++ COMP302/hw7_DiegoCastillo.ml
@@ -0,0 +1,38 @@
+(* SECTION 1 *)
+
+(* Question 1.1 *)
+let rec take (n : int) (s : 'a stream) : 'a list = 
+  if n = 0 then [] else
+    s.head :: take (n - 1) (force s.tail)
+
+(* Question 1.2 *)
+let rec lucas1 = {
+  head = 2;
+  tail = Susp (fun () -> lucas2);
+}
+and lucas2 = {
+  head = 1;
+  tail = Susp (fun () -> add_streams lucas1 lucas2);
+}
+
+
+(* Question 1.3 *)
+let rec unfold (f : 'a -> 'b * 'a) (seed : 'a) : 'b stream = 
+  
+  
+(* Question 1.3 *)
+  let lucas : int stream = int_stream_not_implemented 
+  
+
+
+(* SECTION 2 *)
+
+(* Question 2.1 *)
+let rec scale (s1 : int stream) (n : int) : int stream =
+  raise NotImplemented
+
+let rec merge (s1 : 'a stream) (s2 : 'a stream) : 'a stream =
+  raise NotImplemented
+
+(* Question 2.2 *)
+let rec s = int_stream_not_implemented 
